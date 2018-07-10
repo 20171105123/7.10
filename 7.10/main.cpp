21997cee83623880a797cd1c783328399c52b823
20171105123 <s20171105123@admindeiMac-34.local>
2018-07-10 13:52:16 +0800

    7.10
    这是我的最终代码

--- 7.10/main.cpp
+++ 7.10/main.cpp
@@ -7,9 +7,102 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+#include <fstream>
+using namespace std;
+int main()
+{
+    FILE *a;
+    FILE *b;
+    b=fopen("/Users/S20171105123/Desktop/7.10.csv","a");
+    string s1[200];
+    string s2[200];
+    string s3[200];
+    string s4[200];
+    string s5[200];
+    string s6[200];
+    string s7[200];
+    string s8[200];
+    string s9[200];
+    string s10[200];
+    string s11[200];
+    //int s12[200];
+    
+    
+    if ((a=fopen("/Users/s20171105123/Desktop/7.10/7.10/7.10.csv","r"))==0)
+    {
+        printf("nono\n");
+    }
+    
+    else
+    {
+        for(int i=0;i<3;i++)
+        {
+            fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ，",s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11);
+            
+            //if(i==1||i==2)
+            //{
+            
+            //fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%d ,%d ,%d ,%d ,%d \n",s1,s2,s3,s4,s5,s6,s12[1],s12[2],s12[3],s12[4],s12[5]);
+            
+            //}
+            //else{
+            
+            fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s \n",s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11);
+            printf("%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s \n",s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11);
+            
+            
+        }
+        
+        
+    }
+      return 0;
+}
+void main()
+#include<iostream>
+using namespace std;
+int max (int x,int y);
+int min (int x,int y);
+int main()
+{
+    int a=0,b=101,c=0,i,j;
+    double average;
+    cout<<"请输入选手成绩"<<endl;
+    for(i=1;i<=10;i++)
+    {
+        cout<<"第"<<i<<"位评委给分：";
+        cin>>j;
+        a=max(a,j);
+        b=min(b,j);
+        c=c+j;
+    }
+    average=(c-a-b)/8;
+    cout<<"减去一个最高分"<<a<<","<<"减去一个最低分"<<b<<endl;
+    cout<<"该选手的最后得分："<<average<<endl;
     return 0;
 }
+int max(int x, int y)
+{
+    if(x>y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+int min(int x, int y)
+{
+    if(x>y)
+    {
+        return y;
+    }
+    else
+    {
+        return x;
+    }
+    return 0;
+}
+
+
